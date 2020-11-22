06fdf788cf3e08a7f65dac3ff3238c974dd2c0cd
Quincey James <contact@quinceyjames.com>
2020-11-21 22:43:59 -0500

    bounded scrolling range and added a bounce back animation

--- src/components/horizontal-slider/horizontalSlider.js
+++ src/components/horizontal-slider/horizontalSlider.js
@@ -8,32 +8,49 @@ const pokemonList = [20, 12, 10, 30, 5, 40, 37, 23, 12, 43, 32]
 
 const HorizontalSlider = () => {
   const [pos, setPos] = React.useState(0)
+  const [dragging, setDragging] = React.useState("")
+  const container = React.useRef(null)
   const slider = React.useRef(null)
+
+  const getBoundedPos = (pos) => {
+    const containerSize = container.current.clientWidth;
+    const fullWidth = slider.current.clientWidth;
+    const range = containerSize - fullWidth
+
+    return Math.min(Math.max(pos, range), 0)
+  }
 
   return (
     <div className='horizontal-slider'
-         ref={slider}
+         ref={container}
 
          onMouseDown={event => {
            event.preventDefault()
 
-           const onMouseMove = event => setPos(prev => prev + parseInt(event.movementX));
-           const removeListeners = event => {
-             slider.current.removeEventListener("mousemove", onMouseMove)
-             slider.current.removeEventListener("mouseleave", removeListeners)
-             slider.current.removeEventListener("mouseup", removeListeners)
+           const onMouseMove = event => {
+             setDragging("dragging")
+             setPos(prev => prev + parseInt(event.movementX));
            }
 
-           slider.current.addEventListener("mouseup", removeListeners)
-           slider.current.addEventListener("mouseleave", removeListeners)
+           const removeListeners = event => {
+             setDragging("")
+             setPos(nextPos => {
+               container.current.removeEventListener("mousemove", onMouseMove)
+               container.current.removeEventListener("mouseleave", removeListeners)
+               container.current.removeEventListener("mouseup", removeListeners)
 
-           slider.current.addEventListener("mousemove", onMouseMove)
+               return getBoundedPos(nextPos)
+             })
+           }
+           container.current.addEventListener("mouseup", removeListeners)
+           container.current.addEventListener("mouseleave", removeListeners)
+
+           container.current.addEventListener("mousemove", onMouseMove)
          }}
     >
-      <ul className='slider'
-          style={{
-            transform: `translateX(${pos}px)`
-          }}
+      <ul className={`slider ${dragging}`}
+          ref={slider}
+          style={{transform: `translateX(${pos}px)`}}
       >
         {pokemonList.map((id, i) =>
           <li key={i}>
