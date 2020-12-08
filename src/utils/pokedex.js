31275ab8e1d492da2123e806eb080e1ac0857fa0
lathant <47580015+lathant@users.noreply.github.com>
2020-12-08 09:24:08 -0500

    stats for the stats graph

--- src/utils/pokedex.js
+++ src/utils/pokedex.js
@@ -12,4 +12,17 @@ function getPokemonInfo(id) {
     .catch(console.error)
 }
 
+function summarizePokemonStats(stats) {
+  console.log(stats)
+  let statsArray = []
+  let stat
+   //statsArray will contain array of base stats in this order: [hp,attack,defence,sp.attack,sp.defence,speed]
+  for (stat in stats){
+    statsArray.push(stats[stat].base_stat)
+  }
+
+  return statsArray
+}
+export {getPokemonInfo , summarizePokemonStats}
+
 export {getPokemonInfo}
