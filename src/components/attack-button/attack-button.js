260862f9a93c8484cb360c89331e01d325a19834
pbeaulieu8 <pbeaulieu8@gmail.com>
2020-12-09 01:42:24 -0500

    initial commit

---
+++ src/components/attack-button/attack-button.js
@@ -0,0 +1,162 @@
+import React from "react";
+import "./attack-button.scss";
+
+const TYPES = [
+  "normal",
+  "fire",
+  "water",
+  "electric",
+  "grass",
+  "ice",
+  "fighting",
+  "poison",
+  "ground",
+  "flying",
+  "psychic",
+  "bug",
+  "rock",
+  "ghost",
+  "dragon",
+  "dark",
+  "steel",
+  "fairy",
+];
+const TYPE_ORDER = {
+  normal: 0,
+  fire: 1,
+  water: 2,
+  electric: 3,
+  grass: 4,
+  ice: 5,
+  fighting: 6,
+  poison: 7,
+  ground: 8,
+  flying: 9,
+  psychic: 10,
+  bug: 11,
+  rock: 12,
+  ghost: 13,
+  dragon: 14,
+  dark: 15,
+  steel: 16,
+  fairy: 17,
+};
+const TYPE_CHART = {
+  normal: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0, 1, 1, 0.5, 1],
+  fire: [1, 0.5, 0.5, 1, 2, 2, 1, 1, 1, 1, 1, 2, 0.5, 1, 0.5, 1, 2, 1],
+  water: [1, 2, 0.5, 1, 0.5, 1, 1, 1, 2, 1, 1, 1, 2, 1, 0.5, 1, 1, 1],
+  electric: [1, 1, 2, 0.5, 0.5, 1, 1, 1, 0, 2, 1, 1, 1, 1, 0.5, 1, 1, 1],
+  grass: [1, 0.5, 2, 1, 0.5, 1, 1, 0.5, 2, 0.5, 1, 0.5, 2, 1, 0.5, 1, 0.5, 1],
+  ice: [1, 0.5, 0.5, 1, 2, 0.5, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 0.5, 1],
+  fighting: [2, 1, 1, 1, 1, 2, 1, 0.5, 1, 0.5, 0.5, 0.5, 2, 0, 1, 2, 2, 0.5],
+  poison: [1, 1, 1, 1, 2, 1, 1, 0.5, 0.5, 1, 1, 1, 0.5, 0.5, 1, 1, 0, 2],
+  ground: [1, 2, 1, 2, 0.5, 1, 1, 2, 1, 0, 1, 0.5, 2, 1, 1, 1, 2, 1],
+  flying: [1, 1, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 0.5, 1],
+  psychic: [1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 0.5, 1, 1, 1, 1, 0, 0.5, 1],
+  bug: [1, 0.5, 1, 1, 2, 1, 0.5, 0.5, 1, 0.5, 2, 1, 1, 0.5, 1, 2, 0.5, 0.5],
+  rock: [1, 2, 1, 1, 1, 2, 0.5, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 0.5, 1],
+  ghost: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 1],
+  dragon: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 0.5, 0],
+  dark: [1, 1, 1, 1, 1, 1, 0.5, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 0.5],
+  steel: [1, 0.5, 0.5, 0.5, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0.5, 2],
+  fairy: [1, 0.5, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 1, 1, 1, 2, 2, 0.5, 1],
+};
+
+function effectiveness(
+  moveType,
+  pokemonPrimaryType,
+  pokemonSecondaryType = ""
+) {
+  if (pokemonPrimaryType == "") {
+    return "0px";
+  }
+  var effective_mod = TYPE_CHART[moveType][TYPE_ORDER[pokemonPrimaryType]];
+
+  if (pokemonSecondaryType != "") {
+    effective_mod *= effectiveness(moveType, pokemonSecondaryType);
+  }
+
+  if (effective_mod > 1) {
+    return "5px solid green";
+  } else if (effective_mod < 1) {
+    return "5px solid red";
+  } else {
+    return "0px";
+  }
+}
+
+//change button colour to match move type
+function typeColour(type) {
+  switch (type) {
+    case "water":
+      return "#6390F0";
+    case "fire":
+      return "#EE8130";
+    case "grass":
+      return "#7AC74C";
+    case "electric":
+      return "#F7D02C";
+    case "ice":
+      return "#96D9D6";
+    case "fighting":
+      return "#C22E28";
+    case "poison":
+      return "#A33EA1";
+    case "ground":
+      return "#E2BF65";
+    case "flying":
+      return "#A98FF3";
+    case "psychic":
+      return "#F95587";
+    case "bug":
+      return "#A6B91A";
+    case "rock":
+      return "#B6A136";
+    case "ghost":
+      return "#735797";
+    case "dragon":
+      return "#6F35FC";
+    case "dark":
+      return "#705746";
+    case "steel":
+      return "#B7B7CE";
+    case "fairy":
+      return "#D685AD";
+    //normal type
+    default:
+      return "#A8A77A";
+  }
+}
+
+const AttackButton = ({
+  children,
+  id = "button",
+  targetType = "",
+  targetSecondaryType = "",
+  className,
+  attackName,
+  attackType,
+  color = "white",
+  height,
+  onClick,
+  ...props
+}) => (
+  <a
+    id={id}
+    className={`attack-button ${className}`}
+    style={{
+      height: height,
+      color: color,
+      backgroundColor: typeColour(attackType),
+      border: effectiveness("grass", targetType, targetSecondaryType),
+    }}
+    onClick={onClick}
+    {...props}
+  >
+    {attackName}
+    <br></br>
+    Type: {attackType}
+  </a>
+);
+
+export default AttackButton;
